f44baacfc51e84cd9b69275fa40e1b4cb58a5592
nabaa.naeem <nabaa.naeem@B231-11>
2024-09-06 14:43:46 -0600

    //

--- NabaaProfile210/Program.cs
+++ NabaaProfile210/Program.cs
@@ -9,3 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+
+//
