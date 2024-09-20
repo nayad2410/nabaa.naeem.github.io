a04587270b66b2b2ed6f0c8e0fcc7b4b25dd12d4
nabaa.naeem <nabaa.naeem@B231-11>
2024-09-20 14:33:26 -0600

    the webpage is working now

--- NabaaProfile210/Program.cs
+++ NabaaProfile210/Program.cs
@@ -6,8 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// this was added to give us access to the compontesnt  we will work with
+builder.Services.AddBlazorBootstrap();
 await builder.Build().RunAsync();
 
 
